b6b5841a2259a92ceb5a1dfc5a1adf4b00170add
Doridian <git@doridian.net>
2025-09-16 08:33:52 -0700

    cmdline

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,5 +1,7 @@
 { lib, pkgs, ... }:
 {
+  security.audit.enable = true;
+
   boot = {
     initrd.systemd.enable = true;
 
@@ -7,7 +9,6 @@
 
     kernelPackages = pkgs.linuxPackages_latest;
 
-    security.audit.enable = true;
     kernelParams = ["module.sig_enforce=1" "lsm=landlock,lockdown,yama,integrity,apparmor,bpf" "audit=1" "audit_backlog_limit=256" "lockdown=integrity"];
 
     lanzaboote = {
