a8337aaaa61a3bc81bc8006dade4106c1b5cd4ea
Doridian <git@doridian.net>
2025-09-15 23:29:01 -0700

    use hw

--- flake.nix
+++ flake.nix
@@ -4,15 +4,17 @@
   inputs = {
     lanzaboote.url = "github:nix-community/lanzaboote";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = inputs@{ self, lanzaboote, nixpkgs, ... }:
+  outputs = inputs@{ self, lanzaboote, nixpkgs, nixos-hardware, ... }:
   {
     nixosConfigurations = {
       testvm = nixpkgs.lib.nixosSystem {
         system.stateVersion = "25.05";
 
         modules = [
+          nixos-hardware.nixosModules.qemu-guest
           ./machines/testvm.nix
           lanzaboote.nixosModules.lanzaboote
           ./server.nix
