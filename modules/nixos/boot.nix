257db99c2ee45f6b254cccb3e592e1579c185797
Doridian <git@doridian.net>
2025-09-16 08:33:31 -0700

    cmdline

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -7,6 +7,9 @@
 
     kernelPackages = pkgs.linuxPackages_latest;
 
+    security.audit.enable = true;
+    kernelParams = ["module.sig_enforce=1" "lsm=landlock,lockdown,yama,integrity,apparmor,bpf" "audit=1" "audit_backlog_limit=256" "lockdown=integrity"];
+
     lanzaboote = {
       enable = true;
       pkiBundle = "/etc/secureboot";
