b74bd14f55222227891e8635123ccfdd71b4456e
Doridian <git@doridian.net>
2025-09-17 13:32:55 -0700

    update

--- flake.nix
+++ flake.nix
@@ -11,25 +11,23 @@
   {
     nixosConfigurations =
     let
-      isValidModule = path: nixpkgs.lib.filesystem.pathIsRegularFile path && nixpkgs.lib.strings.hasSuffix ".nix" path;
-      moduleFiles = nixpkgs.lib.filter isValidModule
+      isNixFile = path: nixpkgs.lib.filesystem.pathIsRegularFile path && nixpkgs.lib.strings.hasSuffix ".nix" path;
+
+      moduleFiles = nixpkgs.lib.filter isNixFile
                       (nixpkgs.lib.filesystem.listFilesRecursive ./modules/nixos);
       baseModules = [
         impermanence.nixosModules.impermanence
         lanzaboote.nixosModules.lanzaboote
       ] ++ moduleFiles;
 
-      machineFiles = nixpkgs.lib.filter isValidModule
+      machineFiles = nixpkgs.lib.filter isNixFile
                       (nixpkgs.lib.filesystem.listFilesRecursive ./systems);
-
       mkMachine = path: let
-        elemAt = nixpkgs.lib.strings.elemAt;
+        components = nixpkgs.lib.path.subpath.components
+                      (nixpkgs.lib.strings.removePrefix (toString ./. + "/") (toString path));
 
-        relPath = nixpkgs.lib.strings.removePrefix (toString ./. + "/") (toString path);
-        components = nixpkgs.lib.path.subpath.components (relPath);
-
-        hostname = nixpkgs.lib.strings.removeSuffix ".nix" (elemAt components 2); # e.g. bengalfox;
-        system = elemAt components 1; # e.g. x86_64-linux
+        hostname = nixpkgs.lib.strings.removeSuffix ".nix" (nixpkgs.lib.strings.elemAt components 2); # e.g. bengalfox;
+        system = nixpkgs.lib.strings.elemAt components 1; # e.g. x86_64-linux
       in
       {
         name = hostname;
