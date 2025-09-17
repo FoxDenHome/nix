1802680e2d2c554e84109f2d2e11e50599bc2b84
Doridian <git@doridian.net>
2025-09-16 19:19:48 -0700

    set fish

--- modules/nixos/kanidm.nix
+++ modules/nixos/kanidm.nix
@@ -33,6 +33,7 @@
     unixSettings = {
       pam_allowed_login_groups = [ "login-users" ];
       allow_local_account_override = [ "share" ];
+      default_shell = "${pkgs.fish}/bin/fish";
       uid_attr_map = "name";
       gid_attr_map = "name";
       home_attr = "name";
