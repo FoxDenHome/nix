a6938b8044fe6f7116018006ca68167464abd6e2
Doridian <git@doridian.net>
2025-09-19 09:41:19 -0700

    dont force routes

--- modules/util.nix
+++ modules/util.nix
@@ -3,7 +3,7 @@ let
   mkShortHash = (len: str:
     builtins.substring 0 len (builtins.hashString "sha256" str));
 
-  mkAddrNwInterfaceConfig = cfg: if (cfg.address or null) == null then {} else {
+  mkAddrNwInterfaceConfig = (defgw: cfg: if (cfg.address or null) == null then {} else {
     addresses = [
       {
         address = cfg.address;
@@ -12,20 +12,20 @@ let
     ];
     routes = if (cfg.gateway or null) != null then [
       {
-        address = "default";
+        address = defgw;
         prefixLength = 0;
         via = cfg.gateway;
       }
     ] else [];
-  };
+  });
 in
 {
   mkShortHash = mkShortHash;
   mkHash8 = mkShortHash 8;
 
   mkNwInterfaceConfig = (ifcfg: {
-    ipv4 = mkAddrNwInterfaceConfig ifcfg.ipv4;
-    ipv6 = mkAddrNwInterfaceConfig ifcfg.ipv6;
+    ipv4 = mkAddrNwInterfaceConfig "0.0.0.0" ifcfg.ipv4;
+    ipv6 = mkAddrNwInterfaceConfig "::" ifcfg.ipv6;
   });
 
   mkRoutes = (ifcfg: (if ifcfg.ipv4.gateway != null then [
