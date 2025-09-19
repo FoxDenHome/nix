9c4f97c76a9d305f0b1fb4c8c135fa834d4a2cd6
Doridian <git@doridian.net>
2025-09-19 12:58:46 -0700

    lets see

--- modules/nixos/sops.nix
+++ modules/nixos/sops.nix
@@ -1,4 +1,7 @@
 { lib, config, ... } :
+let
+  sharedSopsFile = ../../secrets/shared.yaml;
+in
 {
   options.foxDen.sops.available = lib.mkEnableOption "Enable sops-nix usage";
 
@@ -8,4 +11,11 @@
 
   config.users.users.root.hashedPasswordFile = lib.mkIf config.foxDen.sops.available config.sops.secrets.rootPasswordHash.path;
   config.users.mutableUsers = ! config.foxDen.sops.available;
+
+  config.nix.extraOptions = lib.mkIf config.foxDen.sops.available ''
+    include ${config.sops.secrets.nixConfig.path}
+  '';
+  config.sops.secrets.nixConfig = lib.mkIf config.foxDen.sops.available {
+    sopsFile = sharedSopsFile;
+  };
 }
