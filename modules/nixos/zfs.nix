4962f5787dd53984c011a61d96e74aae6a6f24b5
Doridian <git@doridian.net>
2025-09-19 09:46:19 -0700

    dont force zfs

--- modules/nixos/zfs.nix
+++ modules/nixos/zfs.nix
@@ -1,10 +1,5 @@
-{ pkgs, ... } :
+{ ... } :
 {
-  boot.supportedFilesystems = [ "zfs" ];
-  environment.systemPackages = with pkgs; [
-    zfs
-  ];
-
   boot.zfs.devNodes = "/dev/disk/by-path";
 
   environment.persistence."/nix/persist/system".files = [
