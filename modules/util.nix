f595777b34327a62a43c2ce2ce2b437ba48a953f
Doridian <git@doridian.net>
2025-09-19 18:57:51 -0700

    manageNW

--- modules/util.nix
+++ modules/util.nix
@@ -3,22 +3,6 @@ let
   mkShortHash = (len: str:
     builtins.substring 0 len (builtins.hashString "sha256" str));
 
-  mkAddrNwInterfaceConfig = (defgw: cfg: if (cfg.address or "") == "" then {} else {
-    addresses = [
-      {
-        address = cfg.address;
-        prefixLength = cfg.prefixLength;
-      }
-    ];
-    routes = if (cfg.gateway or "") != "" then [
-      {
-        address = defgw;
-        prefixLength = 0;
-        via = cfg.gateway;
-      }
-    ] else [];
-  });
-
   mkNetworkdRoutes = (ifcfg: (if (ifcfg.ipv4.gateway or "") != "" then [
     {
       Destination = "0.0.0.0/0";
@@ -33,11 +17,11 @@ let
 
   mkNetworkdAddresses = (ifcfg: (if (ifcfg.ipv4.address or "") != "" then [
     {
-      Address = "${ifcfg.ipv4.address}/${ifcfg.ipv4.prefixLength}";
+      Address = "${ifcfg.ipv4.address}/${toString ifcfg.ipv4.prefixLength}";
     }
   ] else []) ++ (if (ifcfg.ipv6.address or "") != "" then [
     {
-      Address = "${ifcfg.ipv6.address}/${ifcfg.ipv6.prefixLength}";
+      Address = "${ifcfg.ipv6.address}/${toString ifcfg.ipv6.prefixLength}";
     }
   ] else []));
 in
