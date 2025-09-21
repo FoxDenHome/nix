50b162bea26757121ab583f25a69ebc6f7ef6679
Doridian <git@doridian.net>
2025-09-20 23:33:49 -0700

    try samba again

--- systems/x86_64-linux/testvm-fde.nix
+++ systems/x86_64-linux/testvm-fde.nix
@@ -83,7 +83,7 @@ in
 
   foxDen.services.jellyfin.enable = true;
   foxDen.services.jellyfin.tls = false;
-  foxDen.services.samba.enable = false;
+  foxDen.services.samba.enable = true;
 
   systemd.services."getty@tty1" = {
     overrideStrategy = "asDropin";
