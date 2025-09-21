56b41ddc353c98387aa3971f88b9edda9d1a5e43
Doridian <git@doridian.net>
2025-09-20 18:35:19 -0700

    autoreload

--- modules/services.nix
+++ modules/services.nix
@@ -68,7 +68,7 @@ in
       cmd = (eSA "${pkgs.caddy}/bin/caddy");
 
       trustedProxies = config.foxDen.services.trustedProxies;
-      mkTrustedProxies = (prefix: if (builtins.length trustedProxies) > 0 then prefix + " " + (nixpkgs.lib.strings.concatStringsSep " " trustedProxies) else "");
+      mkTrustedProxies = (prefix: if (builtins.length trustedProxies) > 0 then prefix + " " + (nixpkgs.lib.strings.concatStringsSep " " trustedProxies) else "#${prefix} none");
 
       caddyFileEtc = nixpkgs.lib.strings.removePrefix "/etc/" caddyFilePath;
     in
