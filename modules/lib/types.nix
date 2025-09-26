f2532b2b8de485a41acb2fe706ec36a5ebab4d8f
Doridian <git@doridian.net>
2025-09-25 17:32:31 -0700

    Try adding more checks

---
+++ modules/lib/types.nix
@@ -0,0 +1,45 @@
+{ nixpkgs, ... }:
+let
+  types = nixpkgs.lib.types;
+
+  ipv4Check = (ip: let
+    segments = nixpkgs.lib.strings.splitString "." ip;
+    validSegment = (s: let n = nixpkgs.lib.strings.toIntBase10 s; in n >= 0 && n <= 255);
+    allValid = nixpkgs.lib.lists.all validSegment segments;
+  in
+    allValid && (nixpkgs.lib.lists.length segments == 4));
+
+  ipv6Check = (ip: let
+      ipv6 = nixpkgs.lib.network.ipv6.fromString ip;
+    in
+    (builtins.tryEval ipv6).success);
+
+  ipv6CidrCheck = (withCidr: ip: 
+    (ipv6Check ip) && (withCidr == (nixpkgs.lib.strings.hasInfix "/" ip)));
+
+  ipWithCidrCheck = (ipChecker: cidrMax: ipWithCidr: let
+    segments = nixpkgs.lib.strings.splitString "/" ipWithCidr;
+    ip = builtins.elemAt segments 0;
+    cidr = nixpkgs.lib.strings.toIntBase10 (builtins.elemAt segments 1);
+  in
+    (nixpkgs.lib.lists.length segments == 2) && cidr >= 0 && cidr <= cidrMax && (ipChecker ip));
+
+  ipv4WithoutCidr = types.addCheck types.str ipv4Check;
+  ipv4WithCidr = ipWithCidrCheck ipv4Check 32;
+  ipv6WithoutCidr = types.addCheck types.str (ipv6CidrCheck false);
+  ipv6WithCidr = types.addCheck types.str (ipv6CidrCheck true);
+  ipv4 = types.either ipv4WithoutCidr ipv4WithCidr;
+  ipv6 = ipv6Check;
+in
+{
+  ipv4WithoutCidr = ipv4WithoutCidr;
+  ipv4WithCidr = ipv4WithCidr;
+  ipv4 = ipv4;
+  ipv6WithoutCidr = ipv6WithoutCidr;
+  ipv6WithCidr = ipv6WithCidr;
+  ipv6 = ipv6;
+
+  ip = types.either ipv4 ipv6;
+  ipWithCidr = types.either ipv4WithCidr ipv6WithCidr;
+  ipWithoutCidr = types.either ipv4WithoutCidr types.ipv6WithoutCidr;
+}
