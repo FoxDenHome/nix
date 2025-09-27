3181f4968c00a185b0c5c624c72b97b6ea68067d
Doridian <git@doridian.net>
2025-09-27 12:31:58 -0700

    no

--- modules/nixos/services/opensearch.nix
+++ modules/nixos/services/opensearch.nix
@@ -52,8 +52,8 @@ in
       systemd.services.opensearch = {
         serviceConfig = {
           ExecStartPre = [
-            "+${pkgs.coreutils}/bin/mkdir -p /keys"
-            "+${pkgs.openssl}/bin/openssl req -x509 -newkey rsa:2048 -keyout /keys/opensearch.key -out /keys/opensearch.crt -sha256 -days 36500 -nodes -subj '/CN=opensearch'"
+            "${pkgs.coreutils}/bin/mkdir -p /keys"
+            "${pkgs.openssl}/bin/openssl req -x509 -newkey rsa:2048 -keyout /keys/opensearch.key -out /keys/opensearch.crt -sha256 -days 36500 -nodes -subj '/CN=opensearch'"
           ];
 
           ExecStartPost = [ "" ];
