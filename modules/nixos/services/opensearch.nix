dcd34ba7a7811e5f03ad4253049c0cadde6441fc
Doridian <git@doridian.net>
2025-09-27 13:17:49 -0700

    try

--- modules/nixos/services/opensearch.nix
+++ modules/nixos/services/opensearch.nix
@@ -139,8 +139,7 @@ in
             "${pkgs.openssl}/bin/openssl req -x509 -newkey rsa:2048 -keyout /var/lib/opensearch/config/opensearch.key -out /var/lib/opensearch/config/opensearch.crt -sha256 -days 36500 -nodes -subj '/CN=opensearch'"
             "${pkgs.coreutils}/bin/rm -rf /var/lib/opensearch/config/opensearch-security"
             "${pkgs.coreutils}/bin/cp --remove-destination -r /etc/opensearch/security /var/lib/opensearch/config/opensearch-security"
-            "${pkgs.coreutils}/bin/chmod -R 600 /var/lib/opensearch/config/opensearch-security"
-            "${pkgs.coreutils}/bin/chmod 700 /var/lib/opensearch/config/opensearch-security"
+            "${pkgs.coreutils}/bin/chmod -R 700 /var/lib/opensearch/config/opensearch-security"
           ];
 
           ExecStartPost = [ "${pkgs.bash}/bin/bash ${pkgs.opensearch}/plugins/opensearch-security/tools/securityadmin.sh -icl -nhnv -cacert /var/lib/opensearch/config/opensearch.crt -cert /var/lib/opensearch/config/opensearch.crt -key /var/lib/opensearch/config/opensearch.key -cd /var/lib/opensearch/config/opensearch-security" ];
