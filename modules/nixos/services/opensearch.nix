1ae15f243163e3ecfb5f854a33bb47cc24fa1bf3
Doridian <git@doridian.net>
2025-09-28 15:05:24 -0700

    oneshot cant

--- modules/nixos/services/opensearch.nix
+++ modules/nixos/services/opensearch.nix
@@ -179,6 +179,10 @@ in
       };
 
       systemd.services.opensearch = {
+        confinement.packages = [
+          pkgs.gnused
+        ];
+
         serviceConfig = {
           ExecStartPre = [
             "${pkgs.openssl}/bin/openssl req -x509 -newkey rsa:2048 -keyout /var/lib/opensearch/config/opensearch.key -out /var/lib/opensearch/config/opensearch.crt -sha256 -days 36500 -nodes -subj '/CN=opensearch'"
