97de925e9fbc15936923d2769a273d3998de5725
Doridian <git@doridian.net>
2025-09-29 13:12:15 -0700

    timers

--- modules/nixos/services/backupmgr.nix
+++ modules/nixos/services/backupmgr.nix
@@ -15,12 +15,51 @@ let
 in
 {
   options.foxDen.services.backupmgr.enable = lib.mkEnableOption "backupmgr";
+
   config = lib.mkIf svcConfig.enable (lib.mkMerge [
     {
       environment.systemPackages = [
         pkgs.restic
         backupmgrPkg
       ];
+
+      systemd.services.backupmgr-backup = {
+        unitConfig = {
+          Conflicts = [ "backupmgr-prune.service" ];
+        };
+        serviceConfig = {
+          Type = "simple";
+          Restart = "no";
+          ExecStart = [ "${backupmgrPkg}/bin/backupmgr" "--mode=backup" ];
+        };
+      };
+
+      systemd.timers.backupmgr-backup = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnBootSec = "30m";
+          OnUnitActiveSec = "1h";
+        };
+      };
+
+      systemd.services.backupmgr-prune = {
+        unitConfig = {
+          Conflicts = [ "backupmgr-backup.service" ];
+        };
+        serviceConfig = {
+          Type = "simple";
+          Restart = "no";
+          ExecStart = [ "${backupmgrPkg}/bin/backupmgr" "--mode=prune" ];
+        };
+      };
+
+      systemd.timers.backupmgr-prune = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnBootSec = "6h";
+          OnUnitActiveSec = "7d";
+        };
+      };
 
       sops.secrets.backupmgr = config.lib.foxDen.sops.mkIfAvailable {};
 
