a9c42f9facc7d00f2e1c1a94badfb3b59d5a9802
Doridian <git@doridian.net>
2025-09-29 13:04:18 -0700

    backupmgr

---
+++ modules/nixos/services/backupmgr.nix
@@ -0,0 +1,31 @@
+{ foxDenLib, backupmgr, pkgs, lib, config, ... }:
+let
+  services = foxDenLib.services;
+
+  mkDir = (dir: {
+    directory = dir;
+    user = "root";
+    group = "root";
+    mode = "u=rwx,g=,o=";
+  });
+
+  svcConfig = config.foxDen.services.backupmgr;
+
+  backupmgrPkg = backupmgr.packages.${config.nixpkgs.hostPlatform.system}.default;
+in
+{
+  options.foxDen.services.backupmgr.enable = lib.mkEnabledOption "backupmgr";
+
+  sops.secrets.backupmgr = config.lib.foxDen.sops.mkIfAvailable {};
+
+  config = lib.mkIf svcConfig.enable (lib.mkMerge [
+    {
+      environment.systemPackages = [
+        pkgs.restic
+        backupmgrPkg
+      ];
+
+      environment.etc."backupmgr/config.json".source = config.lib.foxDen.sops.mkIfAvailable config.sops.secrets.backupmgr.path;
+    }
+  ]);
+}
