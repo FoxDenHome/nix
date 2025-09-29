046ede8ddadc83dfc9ed7f344677ada49675d971
Doridian <git@doridian.net>
2025-09-29 13:24:44 -0700

    stuff

---
+++ modules/nixos/services/restic-server.nix
@@ -0,0 +1,47 @@
+{ foxDenLib, pkgs, lib, config, ... }:
+let
+  services = foxDenLib.services;
+
+  mkDir = (dir: {
+    directory = dir;
+    user = "restic";
+    group = "restic";
+    mode = "u=rwx,g=,o=";
+  });
+
+  svcConfig = config.foxDen.services.restic-server;
+
+  defaultDataDir = "/var/lib/restic";
+  ifDefaultData = lib.mkIf (config.services.restic.server.dataDir == defaultDataDir);
+in
+{
+  options.foxDen.services.restic-server = services.http.mkOptions { svcName = "restic-server"; name = "Restic Backup Server"; };
+
+  config = lib.mkIf svcConfig.enable (lib.mkMerge [
+    (services.make {
+      name = "restic-rest-server";
+      inherit svcConfig pkgs config;
+    }).config
+    (services.http.make {
+      inherit svcConfig pkgs config;
+      name = "caddy-restic-rest-server";
+      target = "reverse_proxy http://127.0.0.1:8112";
+    }).config
+    {
+      services.restic.server.enable = true;
+      
+      systemd.services.restic-rest-server = {
+        serviceConfig = {
+          StateDirectory = ifDefaultData "restic";
+        };
+      };
+
+      environment.persistence."/nix/persist/restic-server" = ifDefaultData {
+        hideMounts = true;
+        directories = [
+          { directory = defaultDataDir; user = "restic"; group = "restic"; mode = "u=rwx,g=,o="; }
+        ];
+      };
+    }
+  ]);
+}
