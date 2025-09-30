75cfff9ac6cfb84f70fbf86a1a648908e6cb808a
Doridian <git@doridian.net>
2025-09-29 22:41:19 -0700

    Aurbuild step 2

--- modules/lib/services/oci.nix
+++ modules/lib/services/oci.nix
@@ -1,21 +1,17 @@
 { nixpkgs, foxDenLib, ... }:
 let
-    mkNamed = (svc: inputs@{ svcConfig, pkgs, config, ... }: (foxDenLib.services.mkNamed svc inputs) // (let
+    mkNamed = (svc: inputs@{ image, svcConfig, pkgs, config, ... }: (foxDenLib.services.mkNamed svc inputs) // (let
       host = foxDenLib.hosts.getByName config svcConfig.host;
     in {
       config.virtualisation.oci-containers.containers."${svc}" = {
-        image = svcConfig.image;
+        image = image;
         autoStart = true;
         networks = [ "ns:${host.namespacePath}" ];
       };
     }));
 in
 {
-  mkOptions = inputs: (foxDenLib.services.mkOptions inputs) // (with nixpkgs.lib.types; {
-    image = nixpkgs.lib.mkOption {
-      type = str;
-    };
-  });
+  mkOptions = inputs: foxDenLib.services.mkOptions inputs;
   mkNamed = mkNamed;
   make = inputs: mkNamed inputs.name inputs;
 }
