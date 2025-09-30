2ca77f137f22036bdc780c473c101a670a4b090e
Doridian <git@doridian.net>
2025-09-29 22:49:21 -0700

    Mount OCI

--- modules/lib/services/oci.nix
+++ modules/lib/services/oci.nix
@@ -3,11 +3,11 @@ let
     mkNamed = (svc: inputs@{ image, svcConfig, pkgs, config, ... }: (foxDenLib.services.mkNamed svc inputs) // (let
       host = foxDenLib.hosts.getByName config svcConfig.host;
     in {
-      config.virtualisation.oci-containers.containers."${svc}" = {
+      config.services.podman.containers."${svc}" = {
         image = image;
-        autoUpdate = "registry";
         autoStart = true;
         networks = [ "ns:${host.namespacePath}" ];
+        autoUpdate = "registry";
       };
     }));
 in
