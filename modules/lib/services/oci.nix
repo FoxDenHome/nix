21f69956f8c62218ca5873706978efd7d4710115
Doridian <git@doridian.net>
2025-09-29 22:59:47 -0700

    Mount pin

--- modules/lib/services/oci.nix
+++ modules/lib/services/oci.nix
@@ -3,10 +3,16 @@ let
     mkNamed = (svc: inputs@{ image, svcConfig, pkgs, config, ... }: (foxDenLib.services.mkNamed svc inputs) // (let
       host = foxDenLib.hosts.getByName config svcConfig.host;
     in {
+      config.sops.secrets.aurbuildGpgPin = config.lib.foxDen.sops.mkIfAvailable {};
+
       config.virtualisation.oci-containers.containers."${svc}" = {
         image = image;
         autoStart = true;
         networks = [ "ns:${host.namespacePath}" ];
+
+        volumes = config.lib.foxDen.sops.mkIfAvailable  [
+          "${config.sops.secrets.aurbuildGpgPin.path}:/gpg/pin:ro"
+        ];
 
         environment = {
           "GPG_KEY_ID" = "45B097915F67C9D68C19E5747B0F7660EAEC8D49";
