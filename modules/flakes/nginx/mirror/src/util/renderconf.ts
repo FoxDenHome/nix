2726fc8067dc6be8941f8216709e4923f6d6d153
Doridian <git@doridian.net>
2025-09-29 18:42:09 -0700

    dont

--- modules/flakes/nginx/mirror/src/util/renderconf.ts
+++ modules/flakes/nginx/mirror/src/util/renderconf.ts
@@ -1,10 +1,10 @@
 import fs from 'fs';
 
 async function main() {
-    const dir = await fs.promises.readdir('./conf');
+    const dir = await fs.promises.readdir('/njs/conf');
     await fs.promises.mkdir('/tmp/ngxconf', { recursive: true });
     for (const file of dir) {
-        const content = await fs.promises.readFile(`./conf/${file}`);
+        const content = await fs.promises.readFile(`/njs/conf/${file}`);
         const rendered = content.toString()
                             .replace(/__ROOT_DOMAIN__/g, process.env.ROOT_DOMAIN ?? 'mirror.local.foxden.network')
                             .replace(/__ARCH_MIRROR_ID__/g, process.env.ARCH_MIRROR_ID ?? 'archlinux');
