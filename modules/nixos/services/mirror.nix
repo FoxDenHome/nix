60d7bede06f46bb8c6d1a1946e3f7aed7f461c70
Doridian <git@doridian.net>
2025-09-29 19:15:25 -0700

    rsyncd

--- modules/nixos/services/mirror.nix
+++ modules/nixos/services/mirror.nix
@@ -6,9 +6,6 @@ let
   hostCfg = foxDenLib.hosts.getByName config svcConfig.host;
   primaryInterface = lib.lists.head (lib.attrsets.attrValues hostCfg.interfaces);
 
-  defaultDataDir = "/var/lib/mirror";
-  ifDefaultData = lib.mkIf (svcConfig.dataDir == defaultDataDir);
-
   mirrorPkg = nginx-mirror.packages.${config.nixpkgs.hostPlatform.system}.default;
 
   nginxPkg = pkgs.nginxQuic.override {
@@ -25,7 +22,7 @@ in
   options.foxDen.services.mirror = {
     dataDir = lib.mkOption {
       type = lib.types.path;
-      default = defaultDataDir;
+      default = "/var/lib/mirror/data";
       description = "Directory to store mirror data";
     };
 
@@ -38,7 +35,11 @@ in
 
   config = lib.mkIf svcConfig.enable (lib.mkMerge [
     (services.make {
-      name = "mirror";
+      name = "mirror-nginx";
+      inherit svcConfig pkgs config;
+    }).config
+    (services.make {
+      name = "mirror-rsyncd";
       inherit svcConfig pkgs config;
     }).config
     {
@@ -49,17 +50,15 @@ in
       };
       users.groups.mirror = {};
 
-      systemd.services.mirror = {
+      systemd.services.mirror-nginx = {
         confinement.packages = [
           mirrorPkg
         ];
 
         serviceConfig = {
-          BindPaths = [
-            svcConfig.dataDir
-          ];
           BindReadOnlyPaths = [
             "${mirrorPkg}:/njs"
+            "${svcConfig.dataDir}:/data"
           ];
 
           PrivateUsers = false; # needed for the capabilities sadly
@@ -75,21 +74,65 @@ in
           Group = "mirror";
 
           ExecStartPre = [
-            "${pkgs.coreutils}/bin/mkdir -p /var/lib/mirror/ssl"
+            "${pkgs.coreutils}/bin/mkdir -p /var/lib/mirror/ssl ${svcConfig.dataDir}"
             "${pkgs.nodejs_24}/bin/node /njs/lib/util/renderconf.js"
           ];
           ExecStart = [ "${nginxPkg}/bin/nginx -g 'daemon off;' -p /tmp/ngxconf -c nginx.conf" ];
 
-          StateDirectory = ifDefaultData "mirror";
+          StateDirectory = "mirror";
         };
 
         wantedBy = [ "multi-user.target" ];
       };
 
-      environment.persistence."/nix/persist/mirror" = ifDefaultData {
+      environment.etc."foxden/mirror/rsyncd.conf" = {
+        text = ''
+          use chroot = no
+          max connections = 128
+          log file = /dev/stdout
+          pid file = /tmp/rsyncd.pid
+          lock file = /tmp/rsyncd.lock
+          read only = yes
+          numeric ids = yes
+          reverse lookup = no
+          forward lookup = no
+
+          exclude = /.dori-local /_dori-static /.well-known
+
+          [archlinux]
+                  path = /data/archlinux
+
+          [cachyos]
+                  path = /data/cachyos
+
+          [foxdenaur]
+                  path = /data/foxdenaur
+        '';
+      };
+
+      systemd.services.mirror-rsyncd = {
+        serviceConfig = {
+          BindReadOnlyPaths = (foxDenLib.services.mkEtcPaths [
+            "foxden/mirror"
+          ])
+          ++ [
+            "${svcConfig.dataDir}:/data"
+          ];
+          ExecStart = [ "${pkgs.rsync}/bin/rsync --daemon --no-detach --config=/etc/foxden/mirror/rsyncd.conf" ];
+
+          User = "mirror";
+          Group = "mirror";
+
+          StateDirectory = "mirror";
+        };
+
+        wantedBy = [ "multi-user.target" ];
+      };
+
+      environment.persistence."/nix/persist/mirror" = {
         hideMounts = true;
         directories = [
-          { directory = defaultDataDir; user = "mirror"; group = "mirror"; mode = "u=rwx,g=,o="; }
+          { directory = "/var/lib/mirror"; user = "mirror"; group = "mirror"; mode = "u=rwx,g=,o="; }
         ];
       };
     }
