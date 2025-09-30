42c53425d6ae71a716a22892348d05d6e18a8b5d
Doridian <git@doridian.net>
2025-09-29 22:54:50 -0700

    sign

--- modules/lib/services/oci.nix
+++ modules/lib/services/oci.nix
@@ -8,9 +8,9 @@ let
         autoStart = true;
         networks = [ "ns:${host.namespacePath}" ];
 
-        environment = [
-          "GPG_KEY_ID=45B097915F67C9D68C19E5747B0F7660EAEC8D49"
-        ];
+        environment = {
+          "GPG_KEY_ID" = "45B097915F67C9D68C19E5747B0F7660EAEC8D49";
+        };
       };
 
       config.systemd.services."podman-${svc}" = {
