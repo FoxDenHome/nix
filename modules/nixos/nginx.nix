391cf6e56b3cf56cac3a8c4200cc13f5b46f8274
Doridian <git@doridian.net>
2025-10-01 14:00:19 -0700

    nginx

--- modules/nixos/nginx.nix
+++ modules/nixos/nginx.nix
@@ -1,6 +1,6 @@
 { lib, config, ... } :
 {
     environment.etc."foxden/nginx-proxies.conf" = {
-      text = lib.concatStringsSep "\n" (map (ip: "set_real_ip_from ${ip};") config.foxDen.hosts.trustedProxies);
+      text = lib.concatStringsSep "\n" (map (ip: "set_real_ip_from ${ip};") config.foxDen.services.trustedProxies);
     };
 }
