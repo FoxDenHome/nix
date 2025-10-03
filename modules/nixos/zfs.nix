acc426e2b88b64489e39e3c3d65d527e73d5f9dd
Doridian <git@doridian.net>
2025-10-03 14:07:46 -0700

    Add zfs to def

--- modules/nixos/zfs.nix
+++ modules/nixos/zfs.nix
@@ -1,8 +1,12 @@
-{ ... } :
+{ pkgs, ... } :
 {
   boot.zfs.devNodes = "/dev/disk/by-path";
 
   environment.persistence."/nix/persist/system".files = [
     { file = "/etc/zfs/zpool.cache"; }
+  ];
+
+  environment.systemPackages = with pkgs; [
+    zfs
   ];
 }
