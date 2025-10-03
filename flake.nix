3a4b08af181e91f274f83ce91c09f1fcdb9f2aba
Doridian <git@doridian.net>
2025-10-03 10:48:51 -0700

    stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "FoxDen NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     impermanence.url = "github:nix-community/impermanence";
--- modules/lib/global/dns.nix
+++ modules/lib/global/dns.nix
@@ -47,8 +47,9 @@ in
 
   mkRecords = (nixosConfigurations: let
     records = globalConfig.getList ["foxDen" "dns" "records"] nixosConfigurations;
-    horizons = nixpkgs.lib.lists.uniqueStrings (map (record: record.horizon) records);
-    zones = nixpkgs.lib.lists.uniqueStrings (map (record: record.zone) records);
+    # TODO: Go back to uniqueStrings once next NixOS stable
+    horizons = nixpkgs.lib.lists.unique (map (record: record.horizon) records);
+    zones = nixpkgs.lib.lists.unique (map (record: record.zone) records);
   in
   (nixpkgs.lib.attrsets.genAttrs horizons (horizon:
     nixpkgs.lib.attrsets.genAttrs zones (zone:
