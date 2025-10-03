7b42c84706fb969d0da2236e52219d828dc4f191
Doridian <git@doridian.net>
2025-10-03 14:21:34 -0700

    bengalfox is secure

--- systems/x86_64-linux/bengalfox.nix
+++ systems/x86_64-linux/bengalfox.nix
@@ -23,8 +23,8 @@ in
   # These are set when you reinstall the system
   # Change them to "false" for first boot, before secrets exist
   # then, once stuff is done, set them to true
-  foxDen.sops.available = false;
-  foxDen.boot.secure = false;
+  foxDen.sops.available = true;
+  foxDen.boot.secure = true;
 
   system.stateVersion = "25.05";
 
