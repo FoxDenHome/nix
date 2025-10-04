6fc728acc9e8b3001c99a08954752e32545bfabb
Doridian <git@doridian.net>
2025-10-03 17:24:40 -0700

    noetc

--- modules/lib/nginx.nix
+++ modules/lib/nginx.nix
@@ -1,4 +1,4 @@
 { nixpkgs, ... } :
 {
-    mkProxiesConf = config: builtins.toFile "proxies.conf" nixpkgs.lib.concatStringsSep "\n" (map (ip: "set_real_ip_from ${ip};") config.foxDen.services.trustedProxies);
+    mkProxiesConf = config: builtins.toFile "proxies.conf" (nixpkgs.lib.concatStringsSep "\n" (map (ip: "set_real_ip_from ${ip};") config.foxDen.services.trustedProxies));
 }
