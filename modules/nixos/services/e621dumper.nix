c6d8ea292fff591848668fffca5c24f6102199c0
Doridian <git@doridian.net>
2025-10-04 01:22:54 -0700

    fix links

--- modules/nixos/services/e621dumper.nix
+++ modules/nixos/services/e621dumper.nix
@@ -73,6 +73,8 @@ in
 
           Environment = [
             "DOWNLOAD_PATH=${svcConfig.dataDir}"
+            "URL_HOST=${foxDenLib.global.dns.mkHost primaryInterface.dns}"
+            (if svcConfig.tls then "URL_PROTOCOL=https" else "URL_PROTOCOL=http")
             "HOST=127.0.0.1"
             "PORT=8001"
           ];
