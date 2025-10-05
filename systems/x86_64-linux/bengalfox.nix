43849d1de928d6085e38861f592d9539a7978534
Doridian <git@doridian.net>
2025-10-05 14:22:19 -0700

    maybe

--- systems/x86_64-linux/bengalfox.nix
+++ systems/x86_64-linux/bengalfox.nix
@@ -37,6 +37,13 @@ in
   boot.extraModulePackages = [ ];
 
   services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "580.65.06";
+    sha256_64bit = "sha256-BLEIZ69YXnZc+/3POe1fS9ESN1vrqwFy6qGHxqpQJP8=";
+    openSha256 = "sha256-BKe6LQ1ZSrHUOSoV6UCksUE0+TIa0WcCHZv4lagfIgA=";
+    settingsSha256 = "sha256-9PWmj9qG/Ms8Ol5vLQD3Dlhuw4iaFtVHNC0hSyMCU24=";
+    usePersistenced = false;
+  }; # TODO: Remove on 25.11/unstable
   hardware.nvidia.open = true;
   hardware.opengl.enable = true;
 
