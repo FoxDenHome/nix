33b51e8b5ad94bc177ff685ef0c4bc215e96344c
Doridian <git@doridian.net>
2025-10-05 00:00:55 -0700

    exiterr

--- modules/nixos/scripts.nix
+++ modules/nixos/scripts.nix
@@ -2,9 +2,9 @@
 {
   environment.etc."nix/update.sh" = {
     text = ''
-      #!/usr/bin/env sh
-      set -x
-      nix flake update --flake 'github:FoxDenHome/nix'
+      #!/usr/bin/env bash
+      set -xeuo pipefail
+      nix flake update --flake 'github:FoxDenHome/nix' || true
       nixos-rebuild switch --flake "github:FoxDenHome/nix#$(hostname)"
       /etc/nix/sync-boot.sh
     '';
@@ -13,8 +13,8 @@
 
   environment.etc."nix/cleanup.sh" = {
     text = ''
-      #!/usr/bin/env sh
-      set -x
+      #!/usr/bin/env bash
+      set -xeuo pipefail
       nix-collect-garbage --delete-old
       /run/current-system/bin/switch-to-configuration boot
       /etc/nix/sync-boot.sh
@@ -25,8 +25,8 @@
   # TODO: Use a lanzaboote post script for this, once they exist
   environment.etc."nix/sync-boot.sh" = {
     text = ''
-      #!/usr/bin/env sh
-      set -x
+      #!/usr/bin/env bash
+      set -xeuo pipefail
       if [ -d /boot2 ]; then
         rsync -av --delete /boot/ /boot2/
       else
