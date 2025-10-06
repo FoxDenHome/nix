2ac6069c97ddb15f76e415270a37813f9843d3b2
Doridian <git@doridian.net>
2025-10-05 19:18:19 -0700

    apcupsd

--- modules/nixos/services/apcupsd.nix
+++ modules/nixos/services/apcupsd.nix
@@ -19,14 +19,14 @@ in
 
   config = lib.mkIf svcConfig.enable {
     services.apcupsd.enable = true;
-    services.apcupsd.configText = [
-      "UPSNAME ups-rack"
-      "UPSCABLE ether"
-      "UPSTYPE snmp"
-      "NISIP 127.0.0.1"
-      "DEVICE ups-rack.foxden.network:161:APC_NOTRAP:apcupsd"
-      "BATTERYLEVEL ${toString svcConfig.batteryLevel}"
-      "MINUTES ${toString svcConfig.minutes}"
-    ];
+    services.apcupsd.configText = ''
+      UPSNAME ups-rack
+      UPSCABLE ether
+      UPSTYPE snmp
+      NISIP 127.0.0.1
+      DEVICE ups-rack.foxden.network:161:APC_NOTRAP:apcupsd
+      BATTERYLEVEL ${toString svcConfig.batteryLevel}
+      MINUTES ${toString svcConfig.minutes}
+    '';
   };
 }
