c7a3f5fab6925e80780183bd806dc9b5b340a4e9
Doridian <git@doridian.net>
2025-10-08 14:30:32 -0700

    Actually use mkMerge

--- modules/lib/services/oci.nix
+++ modules/lib/services/oci.nix
@@ -1,48 +1,44 @@
-{ foxDenLib, nixpkgs, ... }:
+{ foxDenLib, lib, nixpkgs, ... }:
 let
-    mkNamed = (svc: inputs@{ oci, systemd ? {}, svcConfig, pkgs, config, ... }: (foxDenLib.services.mkNamed "podman-${svc}" inputs) // (let
-      host = foxDenLib.hosts.getByName config svcConfig.host;
-    in {
-      config.virtualisation.oci-containers.containers."${svc}" = nixpkgs.lib.mkMerge [
-        {
-          autoStart = nixpkgs.lib.mkDefault true;
-          pull = nixpkgs.lib.mkDefault "always";
-          networks = [ "host" ];
+    mkNamed = (svc: inputs@{ oci, systemd ? {}, svcConfig, pkgs, config, ... }: (let
+      systemdName = "podman-${svc}";
+    in lib.mkMerge [
+      (foxDenLib.services.mkNamed systemdName inputs)
+      {
+        config.virtualisation.oci-containers.containers."${svc}" = nixpkgs.lib.mkMerge [
+          {
+            autoStart = nixpkgs.lib.mkDefault true;
+            pull = nixpkgs.lib.mkDefault "always";
+            networks = [ "host" ];
 
-          volumes = [
-            "/etc/localtime:/etc/localtime:ro"
-            "/etc/locale.conf:/etc/locale.conf:ro"
-          ];
-          environment = {
-            "TZ" = config.time.timeZone;
-            "LANG" = config.i18n.defaultLocale;
-          };
+            volumes = [
+              "/etc/localtime:/etc/localtime:ro"
+              "/etc/locale.conf:/etc/locale.conf:ro"
+            ];
+            environment = {
+              "TZ" = config.time.timeZone;
+              "LANG" = config.i18n.defaultLocale;
+            };
 
-          podman = {
-            user = svc;
-          };
-        }
-        oci
-      ];
+            podman = {
+              user = svc;
+            };
+          }
+          oci
+        ];
 
-      config.users.users."${svc}" = {
-        isSystemUser = true;
-        group = svc;
-        autoSubUidGidRange = true;
-        home = "/var/lib/foxden-oci/${svc}";
-        createHome = true;
-      };
-      config.users.groups."${svc}" = {};
+        config.users.users."${svc}" = {
+          isSystemUser = true;
+          group = svc;
+          autoSubUidGidRange = true;
+          home = "/var/lib/foxden-oci/${svc}";
+          createHome = true;
+        };
+        config.users.groups."${svc}" = {};
 
-      config.systemd.services."podman-${svc}" = nixpkgs.lib.mkMerge [
-        {
-          requires = [ host.unit ];
-          bindsTo = [ host.unit ];
-          after = [ host.unit ];
-        }
-        systemd
-      ];
-    }));
+        config.systemd.services.${systemdName} = systemd;
+      }
+    ]));
 in
 {
   mkOptions = inputs: foxDenLib.services.mkOptions inputs;
