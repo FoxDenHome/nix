e92ddabdfb1e91fb92f3a46797bb2fe19b4da21c
Doridian <git@doridian.net>
2025-10-08 14:35:11 -0700

    Actually use mkMerge

--- modules/lib/services/oci.nix
+++ modules/lib/services/oci.nix
@@ -2,43 +2,45 @@
 let
     mkNamed = (svc: inputs@{ oci, systemd ? {}, svcConfig, pkgs, config, ... }: (let
       systemdName = "podman-${svc}";
-    in nixpkgs.lib.mkMerge [
-      (foxDenLib.services.mkNamed systemdName inputs)
-      {
-        config.virtualisation.oci-containers.containers."${svc}" = nixpkgs.lib.mkMerge [
-          {
-            autoStart = nixpkgs.lib.mkDefault true;
-            pull = nixpkgs.lib.mkDefault "always";
-            networks = [ "host" ];
+    in {
+      config = nixpkgs.lib.mkMerge [
+        (foxDenLib.services.mkNamed systemdName inputs).config
+        {
+          virtualisation.oci-containers.containers."${svc}" = nixpkgs.lib.mkMerge [
+            {
+              autoStart = nixpkgs.lib.mkDefault true;
+              pull = nixpkgs.lib.mkDefault "always";
+              networks = [ "host" ];
 
-            volumes = [
-              "/etc/localtime:/etc/localtime:ro"
-              "/etc/locale.conf:/etc/locale.conf:ro"
-            ];
-            environment = {
-              "TZ" = config.time.timeZone;
-              "LANG" = config.i18n.defaultLocale;
-            };
+              volumes = [
+                "/etc/localtime:/etc/localtime:ro"
+                "/etc/locale.conf:/etc/locale.conf:ro"
+              ];
+              environment = {
+                "TZ" = config.time.timeZone;
+                "LANG" = config.i18n.defaultLocale;
+              };
 
-            podman = {
-              user = svc;
-            };
-          }
-          oci
-        ];
+              podman = {
+                user = svc;
+              };
+            }
+            oci
+          ];
 
-        config.users.users."${svc}" = {
-          isSystemUser = true;
-          group = svc;
-          autoSubUidGidRange = true;
-          home = "/var/lib/foxden-oci/${svc}";
-          createHome = true;
-        };
-        config.users.groups."${svc}" = {};
+          users.users."${svc}" = {
+            isSystemUser = true;
+            group = svc;
+            autoSubUidGidRange = true;
+            home = "/var/lib/foxden-oci/${svc}";
+            createHome = true;
+          };
+          users.groups."${svc}" = {};
 
-        config.systemd.services.${systemdName} = systemd;
-      }
-    ]));
+          systemd.services.${systemdName} = systemd;
+        }
+      ];
+    }));
 in
 {
   mkOptions = inputs: foxDenLib.services.mkOptions inputs;
