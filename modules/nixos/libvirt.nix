ed9de826194e3c23afda378c84caa4477c79dd1b
Doridian <git@doridian.net>
2025-10-08 20:17:31 -0700

    persist libvirt

--- modules/nixos/libvirt.nix
+++ modules/nixos/libvirt.nix
@@ -3,5 +3,12 @@
   config = {
     virtualisation.libvirtd.qemu.runAsRoot = false;
     virtualisation.libvirtd.enable = true;
+
+    environment.persistence."/nix/persist/libvirt" = {
+      hideMounts = true;
+      directories = [
+        { directory = "/var/lib/libvirt"; user = "libvirt-qemu"; group = "libvirt-qemu"; mode = "u=rwx,g=,o="; }
+      ];
+    };
   };
 }
