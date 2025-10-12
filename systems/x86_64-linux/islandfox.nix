553eab19f0e8de5d39982df9b06183d8efdbf1f1
Doridian <git@doridian.net>
2025-10-12 10:46:25 -0700

    we need this

--- systems/x86_64-linux/islandfox.nix
+++ systems/x86_64-linux/islandfox.nix
@@ -366,4 +366,9 @@ in
       ];
     };
   };
+
+  systemd.services."getty@tty1" = {
+    overrideStrategy = "asDropin";
+    serviceConfig.ExecStart = ["" "@${pkgs.util-linux}/sbin/agetty agetty --login-program ${config.services.getty.loginProgram} --autologin root --noclear --keep-baud %I 115200,38400,9600 $TERM"];
+  };
 }
