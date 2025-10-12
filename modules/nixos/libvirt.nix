4f882d10e0ae4a8a0ca673ab00c524f730bad2a6
Doridian <git@doridian.net>
2025-10-12 12:10:27 -0700

    temp

--- modules/nixos/libvirt.nix
+++ modules/nixos/libvirt.nix
@@ -38,9 +38,15 @@ in
     virtualisation.libvirtd = {
       enable = true;
       qemu = {
+        package = pkgs.qemu_kvm;
         runAsRoot = false;
+        swtpm.enable = true;
         ovmf = {
           enable = true;
+          packages = [(pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd];
         };
       };
     };
