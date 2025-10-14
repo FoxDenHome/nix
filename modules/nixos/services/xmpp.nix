ed3a7bf1d4688c623d0fbc7db7dae14430b93c53
Doridian <git@doridian.net>
2025-10-13 22:14:49 -0700

    maub

--- modules/nixos/services/xmpp.nix
+++ modules/nixos/services/xmpp.nix
@@ -74,6 +74,25 @@ in
           daily_quota = 10 * 1024 * 1024 * 1000;
           expires_after = 60 * 60 * 24 * 7;
         };
+        extraConfig = ''
+          default_storage = "sql"
+
+          sql = {
+            driver = "SQLite3";
+            database = "prosody.sqlite";
+          }
+
+          -- make 0.10-distributed mod_mam use sql store
+          archive_store = "archive2" -- Use the same data store as prosody-modules mod_mam
+
+          storage = {
+            -- this makes mod_mam use the sql storage backend
+            archive2 = "sql";
+          }
+
+          -- https://modules.prosody.im/mod_mam.html
+          archive_expires_after = "1y"
+        '';
         package = pkgs.prosody.override {
           withCommunityModules = [
             "cloud_notify_extensions"
