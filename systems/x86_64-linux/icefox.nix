2a285b14ef71c38ede07bea273294b734a9dfe28
Doridian <git@doridian.net>
2025-10-13 19:01:36 -0700

    snip

--- systems/x86_64-linux/icefox.nix
+++ systems/x86_64-linux/icefox.nix
@@ -542,9 +542,4 @@ in
       ];
     };
   };
-
-  systemd.services."getty@tty1" = {
-    overrideStrategy = "asDropin";
-    serviceConfig.ExecStart = ["" "@${pkgs.util-linux}/sbin/agetty agetty --login-program ${config.services.getty.loginProgram} --autologin root --noclear --keep-baud %I 115200,38400,9600 $TERM"];
-  };
 }
