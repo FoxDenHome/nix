df988d1c4f8481d2ec4b7931a3521614a03d764b
Doridian <git@doridian.net>
2025-10-14 10:39:05 -0700

    dnat

--- systems/x86_64-linux/icefox.nix
+++ systems/x86_64-linux/icefox.nix
@@ -223,7 +223,7 @@ in
         }
 
         chain prerouting {
-          type nat hook prerouting priority -100;
+          type nat hook prerouting priority dstnat; policy accept;
           tcp dport { 80, 443 } ip daddr 95.216.116.140 dnat to 10.99.12.2
           udp dport { 443 } ip daddr 95.216.116.140 dnat to 10.99.12.2
         }
