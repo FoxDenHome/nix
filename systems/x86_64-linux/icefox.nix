ca50d00e27bf2aaff92be244b03d0355e5663cca
Doridian <git@doridian.net>
2025-10-13 20:41:14 -0700

    fwd

--- systems/x86_64-linux/icefox.nix
+++ systems/x86_64-linux/icefox.nix
@@ -233,6 +233,11 @@ in
   #   };
   # };
 
+  boot.kernel.sysctl = {
+    "net.ipv6.conf.all.forwarding" = "1";
+    "net.ipv6.conf.default.forwarding" = "1";
+  };
+
   systemd.network.netdevs."${ifcfg.interface}" = {
     netdevConfig = {
       Name = ifcfg.interface;
