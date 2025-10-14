204278b4a3929e1c0700eace57bc3f01aef6dbef
Doridian <git@doridian.net>
2025-10-14 08:40:47 -0700

    simplify

--- modules/lib/hosts/drivers/routed.nix
+++ modules/lib/hosts/drivers/routed.nix
@@ -4,42 +4,27 @@ let
   mkIfaceName = (interface: "vert-${interface.suffix}");
 in
 {
-  driverOptsType = with nixpkgs.lib.types; submodule {
-    network = nixpkgs.lib.mkOption {
-      type = str;
-    };
-  };
+  driverOptsType = with nixpkgs.lib.types; submodule { };
 
   build = { interfaces, ... } :
   {
-    config.systemd.network.networks = nixpkgs.lib.mkMerge (
-        (map (iface: {
-          "${iface.driverOpts.network}" = {
-            networkConfig = {
-              IPv4Forwarding = true;
-              IPv6Forwarding = true;
-            };
+    config.systemd.network.networks =
+      (nixpkgs.lib.attrsets.listToAttrs (map (iface: {
+        name = "60-vert-${iface.host.name}-${iface.name}";
+        value = {
+          name = mkIfaceName iface;
+          networkConfig = {
+            DHCP = "no";
+            IPv6AcceptRA = false;
+            LinkLocalAddressing = "no";
+            IPv4Forwarding = true;
+            IPv6Forwarding = true;
           };
-        }) interfaces) ++
-        [
-          (nixpkgs.lib.attrsets.listToAttrs
-          (map (iface: {
-              name = "60-vert-${iface.host.name}-${iface.name}";
-              value = {
-                name = mkIfaceName iface;
-                networkConfig = {
-                  DHCP = "no";
-                  IPv6AcceptRA = false;
-                  LinkLocalAddressing = "no";
-                  IPv4Forwarding = true;
-                  IPv6Forwarding = true;
-                };
-                routes = map (addr: {
-                  Destination = addr;
-                }) iface.addresses;
-              };
-            }) interfaces))
-        ]);
+          routes = map (addr: {
+            Destination = addr;
+          }) iface.addresses;
+        };
+      }) interfaces));
   };
 
   hooks = ({ ipCmd, interface, serviceInterface, ... }: let
