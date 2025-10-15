cc309c766f779a7c1e4bcf71c483dd2be2b537a7
Doridian <git@doridian.net>
2025-10-15 15:04:33 -0700

    more records

--- systems/x86_64-linux/dnsrecords/wan.nix
+++ systems/x86_64-linux/dnsrecords/wan.nix
@@ -1,4 +1,43 @@
 { ... } :
+let
+  mkWanRecs = (suffix: [
+    {
+      zone = "foxden.network";
+      name = suffix;
+      type = "A";
+      value = "127.0.0.1";
+      ttl = 300;
+      dynDns = true;
+      horizon = "external";
+    }
+    {
+      zone = "foxden.network";
+      name = suffix;
+      type = "AAAA";
+      value = "::1";
+      ttl = 300;
+      dynDns = true;
+      horizon = "external";
+    }
+    {
+      zone = "foxden.network";
+      name = "v4-${suffix}";
+      type = "A";
+      value = "127.0.0.1";
+      ttl = 300;
+      dynDns = true;
+      horizon = "external";
+    }
+    {
+      zone = "foxden.network";
+      name = "v4-${suffix}";
+      type = "CNAME";
+      value = "${suffix}.foxden.network.";
+      ttl = 300;
+      horizon = "internal";
+    }
+  ]);
+in
 {
   config.foxDen.dns.records = [
     {
@@ -19,11 +58,30 @@
     }
     {
       zone = "foxden.network";
+      name = "vpn";
+      type = "CNAME";
+      ttl = 3600;
+      value = "router.foxden.network.";
+      horizon = "internal";
+    }
+    {
+      zone = "foxden.network";
+      name = "v4-vpn";
+      type = "CNAME";
+      ttl = 3600;
+      value = "router.foxden.network.";
+      horizon = "internal";
+    }
+    {
+      zone = "foxden.network";
       name = "ntp";
       type = "CNAME";
       ttl = 3600;
       value = "wan.foxden.network.";
       horizon = "external";
     }
-  ];
+  ]
+  ++ (mkWanRecs "wan")
+  ++ (mkWanRecs "router")
+  ++ (mkWanRecs "router-backup");
 }
