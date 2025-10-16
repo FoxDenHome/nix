ab6e3337b1ad63b3a620d4eeff5c4d08c6785c72
Doridian <git@doridian.net>
2025-10-16 11:46:26 -0700

    redfox

--- nix/systems/x86_64-linux/dnsrecords/redfox.nix
+++ nix/systems/x86_64-linux/dnsrecords/redfox.nix
@@ -10,7 +10,15 @@
         name = "redfox";
         zone = "foxden.network";
       };
+      cnames = [
+        {
+          name = "redfox";
+          zone = "doridian.net";
+        }
+      ];
       addresses = [
+        "10.99.10.1"
+        "fd2c:f4cb:63be::a63:a01"
         "144.202.81.146"
         "2001:19f0:8001:f07:5400:4ff:feb1:d2e3"
       ];
@@ -25,6 +33,14 @@
       ttl = 3600;
       value = "144.202.81.146";
       horizon = "external";
+    }
+    {
+      zone = "foxden.network";
+      name = "v4-redfox";
+      type = "CNAME";
+      ttl = 3600;
+      value = "redfox.foxden.network.";
+      horizon = "internal";
     }
   ];
 }
