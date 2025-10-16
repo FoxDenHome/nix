2fc232ddd5395dab06fd8d5daf0d0e973791a259
Doridian <git@doridian.net>
2025-10-16 15:59:39 -0700

    en

--- mikrotik/refresh.py
+++ mikrotik/refresh.py
@@ -4,8 +4,8 @@ from refresh.dyndns import refresh_dyndns
 from refresh.foxingress import refresh_foxingress
 
 def main():
-    #print("# DynDNS configuration")
-    #refresh_dyndns()
+    print("# DynDNS configuration")
+    refresh_dyndns()
     print("# foxIngress configuration")
     refresh_foxingress()
 
