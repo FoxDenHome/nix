b584ebbc0fabe32ba8d4b0f64680e6de51f09467
Doridian <git@doridian.net>
2025-10-17 20:25:49 -0700

    This isnt a helper lul

--- nix/systems/x86_64-linux/icefox/network.nix
+++ nix/systems/x86_64-linux/icefox/network.nix
@@ -21,15 +21,65 @@ let
     interface = "br-default";
   };
 
-  routedInterface = foxDenLib.hosts.helpers.hetzner.routedInterface;
-
   phyIface = "enp7s0";
+  routedInterface = "br-routed";
+
+  mkMinHost = (iface: {
+    inherit (ifcfg) nameservers;
+    interfaces.default = iface // {
+      sysctls = {
+        "net.ipv6.conf.INTERFACE.accept_ra" = "0";
+      } // (iface.sysctls or {});
+      addresses = lib.filter (ip: !(foxDenLib.util.isPrivateIP ip)) iface.addresses;
+      driver = "bridge";
+      snirouter.enable = false;
+      driverOpts.bridge = lib.mkDefault ifcfg.interface;
+      driverOpts.vlan = 0;
+      routes = [ ];
+    };
+    interfaces.foxden = iface // {
+      sysctls = {
+        "net.ipv6.conf.INTERFACE.accept_ra" = "0";
+      } // (iface.sysctls or {});
+      mac = null;
+      addresses = lib.filter (foxDenLib.util.isPrivateIP) iface.addresses;
+      driver = "bridge";
+      driverOpts.bridge = ifcfg-foxden.interface;
+      driverOpts.vlan = 0;
+      routes = [
+        { Destination = "10.0.0.0/8"; Gateway = "10.99.12.1"; }
+        { Destination = "fd2c:f4cb:63be::/60"; Gateway = "fd2c:f4cb:63be::a63:c01"; }
+      ];
+    };
+  });
 in
 {
   lib.foxDenSys = {
-    mkHost = foxDenLib.hosts.helpers.hetzner.mkHost ifcfg ifcfg-foxden;
-    mkV6Host = foxDenLib.hosts.helpers.hetzner.mkV6Host ifcfg ifcfg-foxden;
-    mkMinHost = foxDenLib.hosts.helpers.hetzner.mkMinHost ifcfg ifcfg-foxden;
+    inherit routedInterface mkMinHost;
+    mkHost = ifcfg: ifcfg-foxden: iface: lib.mkMerge [
+      (mkMinHost ifcfg ifcfg-foxden iface)
+      {
+        interfaces.default.routes = [
+          { Destination = "0.0.0.0/0"; Gateway = "95.216.116.129"; }
+          { Destination = "::/0"; Gateway = "2a01:4f9:2b:1a42::0:1"; }
+        ];
+      }
+    ];
+    mkV6Host = iface: lib.mkMerge [
+      (mkMinHost ifcfg ifcfg-foxden ({ mac = null; } // iface))
+      {
+        interfaces.default = {
+          dns.auxAddresses = [ "95.216.116.140" ];
+          routes = [
+            { Destination = "::/0"; Gateway = "2a01:4f9:2b:1a42::1:1"; }
+          ];
+          driverOpts.bridge = routedInterface;
+        };
+        interfaces.foxden.routes = [
+          { Destination = "0.0.0.0/0"; Gateway = "10.99.12.1"; }
+        ];
+      }
+    ];
   };
 
   foxDen.hosts.index = 3;
