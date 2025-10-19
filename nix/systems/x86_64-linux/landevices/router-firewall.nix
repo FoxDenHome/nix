173b4e38a16760a633c8f16a8948f576d9a2d2a8
Doridian <git@doridian.net>
2025-10-18 20:26:25 -0700

    name

--- nix/systems/x86_64-linux/landevices/router-firewall.nix
+++ nix/systems/x86_64-linux/landevices/router-firewall.nix
@@ -5,5 +5,5 @@
     chain = "forward";
     action = "accept";
     destination = if foxDenLib.util.isIPv4 rule.source then "10.99.0.0/16" else "fd2c:f4cb:63be::a64:0/112";
-  }) foxDenLib.firewall.templates.trusted "s2s-network";
+  }) (foxDenLib.firewall.templates.trusted "s2s-network");
 }
