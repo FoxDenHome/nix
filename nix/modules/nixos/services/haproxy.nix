b0b56c716518e76fbd524fc42c180d8e9a326509
Doridian <git@doridian.net>
2025-10-19 10:04:36 -0700

    Try haproxy here

---
+++ nix/modules/nixos/services/haproxy.nix
@@ -0,0 +1,29 @@
+{ pkgs, lib, config, foxDenLib, haproxy, ... }:
+let
+  svcConfig = config.foxDen.services.haproxy;
+in
+{
+  options.foxDen.services.haproxy = {
+    config = lib.mkOption {
+      type = lib.types.str;
+      description = "Raw text configuration for HAProxy";
+      default = "";
+    };
+    configFromGateway = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+    };
+  } // foxDenLib.services.mkOptions { svcName = "haproxy"; name = "HAProxy"; };
+
+  config = lib.mkIf svcConfig.enable (lib.mkMerge [
+    (foxDenLib.services.make {
+      inherit pkgs config svcConfig;
+      name = "haproxy";
+    }).config
+    {
+      services.haproxy.config = if svcConfig.configFromGateway != ""
+                                  then haproxy.${svcConfig.configFromGateway}
+                                  else svcConfig.config;
+    }
+  ]);
+}
