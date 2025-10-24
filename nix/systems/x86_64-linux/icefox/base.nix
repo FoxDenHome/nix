12f87fb864de017cb3a51177e93d4de3a08ea1d8
Doridian <git@doridian.net>
2025-10-23 20:31:42 -0700

    network in initrd

--- nix/systems/x86_64-linux/icefox/base.nix
+++ nix/systems/x86_64-linux/icefox/base.nix
@@ -11,7 +11,7 @@
   imports = [ ../../../profiles/server.nix ];
   hardware.enableRedistributableFirmware = true;
   hardware.cpu.amd.updateMicrocode = true;
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
