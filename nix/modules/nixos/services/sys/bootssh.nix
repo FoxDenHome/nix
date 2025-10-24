76bb75a6b9dd386ab5013e913e9855ead3c5c9dd
Doridian <git@doridian.net>
2025-10-24 11:50:10 -0700

    other port so host key check doesnt freak

--- nix/modules/nixos/services/sys/bootssh.nix
+++ nix/modules/nixos/services/sys/bootssh.nix
@@ -10,6 +10,7 @@ in
   config = lib.mkIf config.foxDen.services.bootssh.enable {
     boot.initrd.network.ssh = {
       enable = true;
+      port = 4; # 2+2 :3
       authorizedKeys.keys = [
         "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBN/lwUU7uvM/G/4HdEg4SJ2YaEq2SK8mwMcdPmRZBQ6w4+RykvUAJi3cVLBeI8Ng3vW3faDIW2HU2P4qF4y+kDM= doridian@fennec"
         "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNhmxwlcA26Dee8lNV3rSDxZeC+Q8xDC80iwLDO9d9smSZ4qBAcLGZrjV4mRyVI0omNG/VyB10VCQrS52E+QqrQ= doridian@capefox"
