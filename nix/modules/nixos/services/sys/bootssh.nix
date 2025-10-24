70da126102620f3281e2bfc285bfc56ffcfab776
Doridian <git@doridian.net>
2025-10-24 11:45:23 -0700

    autokeygen

--- nix/modules/nixos/services/sys/bootssh.nix
+++ nix/modules/nixos/services/sys/bootssh.nix
@@ -1,4 +1,8 @@
 { lib, config, pkgs, ... } :
+let
+  rsaKeyFile = "/nix/persist/foxden/bootssh/ssh_host_rsa_key";
+  ed25519KeyFile = "/nix/persist/foxden/bootssh/ssh_host_ed25519_key";
+in
 {
   options.foxDen.services.bootssh = {
     enable = lib.mkEnableOption "bootssh";
@@ -10,12 +14,9 @@
         "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBN/lwUU7uvM/G/4HdEg4SJ2YaEq2SK8mwMcdPmRZBQ6w4+RykvUAJi3cVLBeI8Ng3vW3faDIW2HU2P4qF4y+kDM= doridian@fennec"
         "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNhmxwlcA26Dee8lNV3rSDxZeC+Q8xDC80iwLDO9d9smSZ4qBAcLGZrjV4mRyVI0omNG/VyB10VCQrS52E+QqrQ= doridian@capefox"
       ];
-      # mkdir -p /nix/persist/foxden/bootssh
       hostKeys = [
-        # ssh-keygen -t rsa -N '' -f /nix/persist/foxden/bootssh/ssh_host_rsa_key
-        "/nix/persist/foxden/bootssh/ssh_host_rsa_key"
-        # ssh-keygen -t ed25519 -N '' -f /nix/persist/foxden/bootssh/ssh_host_ed25519_key
-        "/nix/persist/foxden/bootssh/ssh_host_ed25519_key"
+        rsaKeyFile
+        ed25519KeyFile
       ];
     };
     boot.initrd.systemd = {
@@ -23,6 +24,20 @@
       storePaths = with pkgs; [
         bash
         cryptsetup
+      ];
+    };
+    system.activationScripts.bootsshKeygen = {
+      text = ''
+        mkdir -p /nix/persist/foxden/bootssh
+        if [ ! -f ${rsaKeyFile} ]; then
+          ssh-keygen -t rsa -N "" -f ${rsaKeyFile}
+        fi
+        if [ ! -f ${ed25519KeyFile} ]; then
+          ssh-keygen -t ed25519 -N "" -f ${ed25519KeyFile}
+        fi
+      '';
+      deps = [
+        "users"
       ];
     };
   };
