083eac08502ead2b1b2c0aabc8f86330502e046c
Doridian <git@doridian.net>
2025-10-26 19:32:24 -0700

    alphasort

--- nix/flake.nix
+++ nix/flake.nix
@@ -11,27 +11,27 @@
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-    uds-proxy.url = "github:Doridian/uds-proxy";
-    uds-proxy.inputs.nixpkgs.follows = "nixpkgs";
+    backupmgr.url = "github:FoxDenHome/backupmgr";
+    backupmgr.inputs.nixpkgs.follows = "nixpkgs";
+    e621dumper.url = "github:FoxDenHome/e621dumper";
+    e621dumper.inputs.nixpkgs.follows = "nixpkgs";
     fadumper.url = "github:Doridian/fadumper";
     fadumper.inputs.nixpkgs.follows = "nixpkgs";
     gitbackup.url = "github:Doridian/gitbackup";
     gitbackup.inputs.nixpkgs.follows = "nixpkgs";
-    superfan.url = "github:Doridian/superfan";
-    superfan.inputs.nixpkgs.follows = "nixpkgs";
-    e621dumper.url = "github:FoxDenHome/e621dumper";
-    e621dumper.inputs.nixpkgs.follows = "nixpkgs";
-    backupmgr.url = "github:FoxDenHome/backupmgr";
-    backupmgr.inputs.nixpkgs.follows = "nixpkgs";
     oauth-jit-radius.url = "github:Doridian/oauth-jit-radius";
     oauth-jit-radius.inputs.nixpkgs.follows = "nixpkgs";
     spaceage-api.url = "github:SpaceAgeMP/space_age_api";
     spaceage-api.inputs.nixpkgs.follows = "nixpkgs";
-    spaceage-website.url = "github:SpaceAgeMP/website";
     spaceage-tts.url = "github:SpaceAgeMP/TTS";
     spaceage-tts.inputs.nixpkgs.follows = "nixpkgs";
+    spaceage-website.url = "github:SpaceAgeMP/website";
+    superfan.url = "github:Doridian/superfan";
+    superfan.inputs.nixpkgs.follows = "nixpkgs";
     tapemgr.url = "github:FoxDenHome/tapemgr";
     tapemgr.inputs.nixpkgs.follows = "nixpkgs";
+    uds-proxy.url = "github:Doridian/uds-proxy";
+    uds-proxy.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs: import ./outputs.nix inputs;
