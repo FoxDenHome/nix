938861551f31a80d152c8b108b0a3c586afec556
Doridian <git@doridian.net>
2025-10-26 20:38:47 -0700

    syncoid  target user

--- nix/systems/x86_64-linux/icefox/users.nix
+++ nix/systems/x86_64-linux/icefox/users.nix
@@ -11,4 +11,17 @@
   users.groups.kilian = {
     gid = 1009;
   };
+
+  users.users.bengalfox = {
+    isNormalUser = true;
+    autoSubUidGidRange = false;
+    group = "bengalfox";
+    uid = 1003;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFHFHMI5pnBacMCpf2TnOluiqOCCQVYs0hnH2ZpicIUV root@bengalfox"
+    ];
+  };
+  users.groups.bengalfox = {
+    gid = 1003;
+  };
 }
