eba4f9e9931efbcc339e75baee9477c400b98c17
Doridian <git@doridian.net>
2025-10-28 13:25:37 -0700

    mv

--- nix/systems/x86_64-linux/bengalfox/zfs.nix
+++ nix/systems/x86_64-linux/bengalfox/zfs.nix
@@ -36,7 +36,7 @@ in
       enable = true;
       commands.zhdd = {
         source = "zhdd/ROOT";
-        target = "bengalfox@v4-icefox.doridian.net:ztank/ROOT/zhdd";
+        target = "bengalfox@v4-icefox.doridian.net:ztank/ROOT/bengalfox";
         sshKey = config.sops.secrets."syncoid-ssh-key".path;
         recursive = true;
       };
