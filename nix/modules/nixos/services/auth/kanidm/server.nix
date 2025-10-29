e0873c8332a559a4e1374f5c9c909de07b519a23
Doridian <git@doridian.net>
2025-10-28 20:24:49 -0700

    set

--- nix/modules/nixos/services/auth/kanidm/server.nix
+++ nix/modules/nixos/services/auth/kanidm/server.nix
@@ -142,6 +142,7 @@ in
           ExecStartPost = config.lib.foxDen.sops.mkIfAvailable [
             (pkgs.writeShellScript "set-posix-attrs" ''
               export KANIDM_PASSWORD="$(cat ${config.sops.secrets."kanidm-idm_admin-password".path})"
+              ${config.services.kanidm.package}/bin/kanidm login --name idm_admin
               ${config.services.kanidm.package}/bin/kanidm person posix set --name idm_admin doridian --gidnumber 2006
               ${config.services.kanidm.package}/bin/kanidm person posix set --name idm_admin wizzy --gidnumber 2010
               ${config.services.kanidm.package}/bin/kanidm group posix set --name idm_admin login-users --gidnumber 4242
