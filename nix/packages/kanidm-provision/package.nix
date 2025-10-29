b116fac25264e7d006cab7aaa100cca57e4086e4
Doridian <git@doridian.net>
2025-10-29 11:15:20 -0700

    nah

--- nix/packages/kanidm-provision/package.nix
+++ nix/packages/kanidm-provision/package.nix
@@ -1,17 +1,19 @@
 { lib, pkgs, ... }:
 pkgs.rustPlatform.buildRustPackage (finalAttrs: {
   pname = "kanidm-provision";
-  version = "0.3.0";
+  version = "1.3.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "Doridian";
     repo = "kanidm-provision";
-    rev = "139c2762e77e10cb2327d76f2f8d99e91e4cb07b";
+    rev = "5c642b0c4fe214147c4907b04979db30a0ea311b";
     hash = "sha256-m3bF4wFPVRc2E+E/pZc3js9T4rYbTejo/FFpysytWKw=";
   };
 
   cargoHash = "sha256-dPTrIc/hTbMlFDXYMk/dTjqaNECazldfW43egDOwyLM=";
 
+  nativeInstallCheckInputs = [ pkgs.versionCheckHook ];
+  versionCheckProgramArg = "--version";
   doInstallCheck = true;
 
   passthru = {
