5b0a95615811d62aa785ab02e639437f48a7ccc4
Doridian <git@doridian.net>
2025-10-30 11:48:53 -0700

    Enhance

---
+++ nix/modules/lib/hosts/drivers/sriov.nix
@@ -0,0 +1,63 @@
+{ nixpkgs, ... } :
+let
+  eSA = nixpkgs.lib.strings.escapeShellArg;
+in
+{
+  driverOptsType = with nixpkgs.lib.types; submodule {
+    vlan = nixpkgs.lib.mkOption {
+      type = ints.unsigned;
+    };
+    root = nixpkgs.lib.mkOption {
+      type = str;
+    };
+  };
+
+  build = { ... } : { };
+
+  hooks = ({ pkgs, ipCmd, defaultServiceInterface, interface, ... }: let
+    root = interface.driverOpts.root;
+
+    allocSriovScript = pkgs.writeShellScript "allocate-sriov" ''
+      set -euox pipefail
+      interface="$1"
+      numvfs_file="/sys/class/net/${root}/device/sriov_numvfs"
+      totalvfs="$(cat /sys/class/net/${root}/device/sriov_totalvfs)"
+
+      assign_vf() {
+        idx="$1"
+        # Enable spoof checking, set MAC and VLAN
+        ${ipCmd} link set dev "${root}" vf "$idx" spoofchk on mac "${interface.mac}" vlan "${builtins.toString interface.driverOpts.vlan}"
+        # Find current name of VF interface
+        ifname="$(${pkgs.coreutils}/bin/ls /sys/class/net/${root}/device/virtfn$idx/net/)"
+        # And rename it
+        ${ipCmd} link set dev "$ifname" name "${defaultServiceInterface}"
+      }
+
+      # Condition A: We find a VIF with our MAC address
+      matched_vf="$(${ipCmd} link show dev "${root}" | ${pkgs.gnugrep}/bin/grep -oi "vf .* link/ether ${interface.mac} " | ${pkgs.coreutils}/bin/cut -d' ' -f2)"
+      if [ -n "$matched_vf" ]; then
+        assign_vf "$matched_vf"
+        exit 0
+      fi
+
+      # Condition B: numvfs < totalvfs, we can allocate a new one
+      current_vfs="$(cat "$numvfs_file")"
+      if [ "$current_vfs" -lt "$totalvfs" ]; then
+        echo $((current_vfs + 1)) > "$numvfs_file"
+        assign_vf "$current_vfs"
+        exit 0
+      fi
+
+      # TODO: Condition C: No free VFs, go hunting for unused ones
+      exit 1
+    '';
+  in
+  {
+    start = [
+      "${pkgs.util-linux}/bin/flock -x /run/foxden-sriov.lock ${allocSriovScript} ${eSA interface.driverOpts.root}"
+    ];
+    serviceInterface = "";
+    stop = [
+    ];
+  });
+}
