028a8ee3835278cb12d2f8d3e3afc07c9de9bbcf
Doridian <git@doridian.net>
2025-10-29 19:46:35 -0700

    immich

---
+++ nix/systems/x86_64-linux/islandfox/immich.nix
@@ -0,0 +1,26 @@
+{ config, ... }:
+let
+  mkVlanHost = config.lib.foxDenSys.mkVlanHost;
+in
+{
+  foxDen.services = config.lib.foxDen.sops.mkIfAvailable {
+    immich = {
+      enable = true;
+      host = "immich";
+    };
+  };
+
+  foxDen.hosts.hosts = {
+    immich = mkVlanHost 2 {
+      dns = {
+        name = "images.foxden.network";
+        dynDns = true;
+      };
+      webservice.enable = true;
+      addresses = [
+        "10.2.11.23/16"
+        "fd2c:f4cb:63be:2::b17/64"
+      ];
+    };
+  };
+}
