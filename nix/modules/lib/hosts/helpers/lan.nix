30a2bfd46363111095127135ab98de94635f2d15
Doridian <git@doridian.net>
2025-10-30 12:28:01 -0700

    split config

--- nix/modules/lib/hosts/helpers/lan.nix
+++ nix/modules/lib/hosts/helpers/lan.nix
@@ -9,16 +9,20 @@ rec {
     { Destination = "0.0.0.0/0"; Gateway = "10.${builtins.toString vlan}.0.1"; }
   ]);
 
-  mkVlanHost = (ifcfg: vlan: cfg: {
+  mkVlanHost = (ifcfg: vlan: cfg: let
+    driver = ifcfg.defaultDriver or "bridge";
+  in {
     nameservers = mkNameservers vlan;
     interfaces.default = {
-      driver = ifcfg.defaultDriver or "bridge";
-      driverOpts = {
-        bridge = ifcfg.interface;
+      inherit driver;
+      driverOpts = if driver == "sriov" then {
         root = ifcfg.phyIface;
         rootPvid = ifcfg.phyPvid;
         vlan = vlan;
-      };
+      } else if driver == "bridge" then {
+        bridge = ifcfg.interface;
+        vlan = vlan;
+      } else {};
       routes = mkRoutes vlan;
     } // cfg;
   });
