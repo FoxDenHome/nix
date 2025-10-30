c791861ec0c9691a6042e690acb9cd7be034f866
Doridian <git@doridian.net>
2025-10-29 20:25:14 -0700

    redis

--- nix/modules/nixos/services/nas/immich.nix
+++ nix/modules/nixos/services/nas/immich.nix
@@ -74,7 +74,6 @@ in
             BindReadOnlyPaths = [
               config.services.immich.environment.IMMICH_CONFIG_FILE
             ];
-            StateDirectory = lib.mkForce "immich";
           };
         };
       in
