3fc8089953e116b6e4b1ad53b52406febdd6e645
Doridian <git@doridian.net>
2025-10-30 12:40:25 -0700

    implement harvesting

--- nix/modules/lib/hosts/drivers/sriov.nix
+++ nix/modules/lib/hosts/drivers/sriov.nix
@@ -26,8 +26,8 @@
       interface="$1"
       numvfs_file="/sys/class/net/${root}/device/sriov_numvfs"
       totalvfs="$(cat /sys/class/net/${root}/device/sriov_totalvfs)"
-      current_vfs="$(cat "$numvfs_file")"
-      if [ "$current_vfs" -eq 0 ]; then
+      numvfs="$(cat "$numvfs_file")"
+      if [ "$numvfs" -eq 0 ]; then
         echo $totalvfs > "$numvfs_file"
       fi
 
@@ -56,7 +56,15 @@
       # Condition B: Find an unused VF
       assign_vf_by_mac '00:00:00:00:00:00'
 
-      # TODO: Condition C: No free VFs, go hunting for unused ones (in main netns)
+      # Condition C: No free VFs, go hunting for unused ones (in main netns)
+      for i in `seq 0 $(( $numvfs - 1 ))`; do
+        # If the interface is listed here with its name, it is in the root NS, so it is unused
+        ifname="$(${pkgs.coreutils}/bin/ls /sys/class/net/${root}/device/virtfn$i/net/)"
+        if [ -z "$ifname" ]; then
+          assign_vf "$i"
+          exit 0
+        fi
+      done
       exit 1
     '';
   in
