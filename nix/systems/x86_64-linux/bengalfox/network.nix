a8a8a209772971000a6e432a0453b88fe6a9ad1d
Doridian <git@doridian.net>
2025-10-31 12:43:17 -0700

    Reapply "Actually, we want bridgies"
    This reverts commit a88e423140abb1d52d23ac2795f359965baf350e.

--- nix/systems/x86_64-linux/bengalfox/network.nix
+++ nix/systems/x86_64-linux/bengalfox/network.nix
@@ -8,6 +8,7 @@ let
     mtu = 9000;
     routes = foxDenLib.hosts.helpers.lan.mkRoutes 2;
     nameservers = foxDenLib.hosts.helpers.lan.mkNameservers 2;
+    interface = "br-default";
     phyIface = "ens1f0np0";
     phyPvid = 2;
     defaultDriver = "sriov";
@@ -18,9 +19,10 @@ in
 
   foxDen.hosts.index = 1;
   foxDen.hosts.gateway = "router";
+  virtualisation.libvirtd.allowedBridges = [ ifcfg.interface ];
 
-  systemd.network.networks."30-${ifcfg.phyIface}" = {
-    name = ifcfg.phyIface;
+  systemd.network.networks."30-${ifcfg.interface}" = {
+    name = ifcfg.interface;
     routes = ifcfg.routes;
     address = ifcfg.addresses;
     dns = ifcfg.nameservers;
@@ -30,11 +32,44 @@ in
       IPv6AcceptRA = true;
     };
 
+    bridgeVLANs = [{
+      PVID = ifcfg.phyPvid;
+      EgressUntagged = ifcfg.phyPvid;
+      VLAN = builtins.toString ifcfg.phyPvid;
+    }];
+
     linkConfig = {
       MTUBytes = ifcfg.mtu;
     };
   };
-  #boot.initrd.systemd.network.networks."30-${ifcfg.phyIface}" = config.systemd.network.networks."30-${ifcfg.phyIface}";
+  #boot.initrd.systemd.network.networks."30-${ifcfg.phyIface}" = config.systemd.network.networks."30-${ifcfg.interface}" // { name = ifcfg.phyIface; };
+
+  systemd.network.netdevs."${ifcfg.interface}" = {
+    netdevConfig = {
+      Name = ifcfg.interface;
+      Kind = "bridge";
+      MACAddress = ifcfg.mac;
+    };
+
+    bridgeConfig = {
+      VLANFiltering = true;
+    };
+  };
+
+  systemd.network.networks."40-${ifcfg.interface}-root" = {
+    name = ifcfg.phyIface;
+    bridge = [ifcfg.interface];
+
+    bridgeVLANs = [{
+      PVID = ifcfg.phyPvid;
+      EgressUntagged = ifcfg.phyPvid;
+      VLAN = "1-10";
+    }];
+
+    linkConfig = {
+      MTUBytes = ifcfg.mtu;
+    };
+  };
 
   foxDen.hosts.hosts = {
     bengalfox = {
