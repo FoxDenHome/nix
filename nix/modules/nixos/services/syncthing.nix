c69a2820a6fe5a806c50323c0cf213645a3e65b8
Doridian <git@doridian.net>
2025-10-31 20:30:14 -0700

    secrets

--- nix/modules/nixos/services/syncthing.nix
+++ nix/modules/nixos/services/syncthing.nix
@@ -32,10 +32,6 @@ in
     (services.http.make {
       inherit svcConfig pkgs config;
       name = "http-syncthing";
-      package = pkgs.caddy.withPlugins {
-        plugins = [ "github.com/mholt/caddy-webdav@v0.0.0-20250805175825-7a5c90d8bf90" ];
-        hash = "sha256-FOs4Y6UZWmUHDYWdKoqcU8k6hodISYS03BQkGx76OpU=";
-      };
       modules = [
         pkgs.nginxModules.dav
       ];
